b9179a9d007f5b0b22795832c07b217911d3d3ad
Ben Corriette <benjamin.corriette@bison.howard.edu>
2021-09-07 18:24:46 +0000

    Linked List updates

--- list/list.c
+++ list/list.c
@@ -2,21 +2,151 @@
 // 
 // Implementation for linked list.
 //
-// <Author>
+// Ben Corriette
 
 #include <stdio.h>
+#include <stdlib.h>
+//#include <string.h>
 
 #include "list.h"
 
-list_t *list_alloc() { return NULL; }
-void list_free(list_t *l) {}
+// Dynamically allocate heap memory for a linked list
+list_t *list_alloc() {
+  list_t * list = (list_t *) malloc(sizeof(list_t));
+  return list;
+}
 
-void list_print(list_t *l) {}
-int list_length(list_t *l) { return -1; }
+// Dynamically free heap memory from a linked list
+void list_free(list_t *l) {
+  free(l);
+  return;
+}
 
-void list_add_to_back(list_t *l, elem value) {}
-void list_add_to_front(list_t *l, elem value) {}
-void list_add_at_index(list_t *l, elem value, int index) {}
+// Print out the linked list
+void list_print(list_t *l) {
+	printf("Linked list: {");
+	// Start from the first element, then matriculate through
+	if (l->head) {
+		printf("%d", l->head->value);
+		node_t * nextNode;
+		if (l->head->next) {
+			nextNode = l->head->next;
+			printf(", %d", nextNode->value);
+			while(nextNode->next != NULL) {
+        printf(", %d", nextNode->next->value);
+				*nextNode = *nextNode->next;
+			}
+		}
+	}
+	printf("}\n");
+	return;
+}
+
+// Return the length of the list
+int list_length(list_t *l) {
+	// Start counting from the first element onwards
+	int i = 0;
+	if (l->head) {
+		i++;
+		node_t * nextNode;
+		if (l->head->next) {
+			i++;
+			nextNode = l->head->next;
+			while (nextNode->next) {
+				nextNode = nextNode->next;
+				i++;
+			}
+		}
+	}
+	return i;
+}
+
+// Add element to the back of a linked list
+void list_add_to_back(list_t *l, elem value) {
+	// Iterate through all elements then add another one at the back
+	if (l->head) {
+		node_t * nextNode;
+		if (l->head->next) {
+			nextNode = l->head->next;
+			while (nextNode->next) {
+				nextNode = nextNode->next;
+			}
+			node_t * lastNode;
+			lastNode->value = value;
+			nextNode->next = lastNode;
+		}
+	}
+	return;
+}
+
+// Add element to the front of a linked list
+void list_add_to_front(list_t *l, elem value) {
+	// Add new first node to copy of original linked list
+	list_t * newList;
+	node_t * newFirstNode;
+	newFirstNode->value = value;
+	newList->head = newFirstNode;
+	
+	if (l->head) {
+		newList->head->next = l->head;
+		if (l->head->next) {
+			node_t * nextNode = l->head->next;
+			node_t * nextNewListNode = newList->head->next->next;
+			nextNewListNode = nextNode;
+			while (nextNode->next) {
+				nextNewListNode->next = nextNode->next;
+			}
+		}
+		l = newList;
+	}
+	return;
+}
+
+// Add element at a specific index in the linked list
+void list_add_at_index(list_t *l, elem value, int index) {
+	// Add new node to copy of original linked list
+	int i = 0;
+	list_t * newList;
+	node_t * newNode;
+	newNode->value = value;
+	if (index == 0) {
+		newList->head = newNode;
+		i++;
+	}
+	if (l->head) {
+		if (index > 0 && i == 0) {
+			newList->head = l->head;
+			i++;
+		}
+		if (index == 1) {
+			newList->head->next = newNode;
+			i++;
+		}
+		else if (index == 0 && i > 0) {
+			newList->head->next = l->head;
+			i++;
+		}
+		node_t * nextNode = l->head->next;
+		node_t * nextNewListNode;
+		if (index < 2) {
+			nextNewListNode = newList->head->next->next;
+		}
+		else {
+			nextNewListNode = newList->head->next;
+		}
+		while (nextNode->next) {
+			i++;
+			if (index != i) {
+				nextNewListNode->next = nextNode->next;
+			}
+			else {
+				nextNewListNode->next = newNode;
+			}
+		}
+		l = newList;
+	}
+	return;
+}
 
 elem list_remove_from_back(list_t *l) { return -1; }
 elem list_remove_from_front(list_t *l) { return -1; }
--- list/main.c
+++ list/main.c
@@ -2,7 +2,48 @@
 
 #include "list.h"
 
+// Testing implemented functions
 int main() {
-  printf("Tests for linked list implementation\n");
+	list_t testList;
+	node_t startNode;
+	node_t nodeTwo;
+	node_t nodeThree;
+	node_t nodeFour;
+	node_t nodeFive;
+	
+	testList.head = &startNode;
+	
+	startNode.value = 1;
+	nodeTwo.value = 2;
+	nodeThree.value = 3;
+	nodeFour.value = 4;
+	nodeFive.value = 5;
+	
+	startNode.next = &nodeTwo;
+	nodeTwo.next = &nodeThree;
+	nodeThree.next = &nodeFour;
+	nodeFour.next = &nodeFive;
+	
+  printf("Testing list_alloc()...\n");
+	list_t * dynamicList = list_alloc();
+	
+	printf("Testing list_free(list_t *l)...\n");
+	list_free(dynamicList);
+	
+	printf("Testing list_print(list_t *l)...\n");
+	list_print(&testList);
+	
+	/*printf("Testing list_alloc()\n");
+	list_alloc();
+	
+	printf("Testing list_alloc()\n");
+	list_alloc();
+	
+	printf("Testing list_alloc()\n");
+	list_alloc();
+	
+	printf("Testing list_alloc()\n");
+	list_alloc();*/
+	
   return 0;
 }
